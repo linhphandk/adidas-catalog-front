fc49d4e7acd876689b8c1fc9677b7f5540bcc9ce
Thach linhphan <lphan@iraiser.eu>
2021-02-22 20:20:25 +0100

    [FIX]: page switcher margin and cursor pointer

--- src/features/Pagination.style.ts
+++ src/features/Pagination.style.ts
@@ -6,12 +6,15 @@ export const StyledPagination = styled.div`
 export const StyledPageSwitcher = styled.div`
   display:flex;
   align-items: center;
+  margin: 0 15px;
+  cursor: pointer;
 `;
 
 StyledPageSwitcher.displayName = 'StyledPageSwitcher';
 export const StyledPaginationItem = styled.div`
   padding: 10px;
   border-radius: 5px;
+  cursor: pointer;
   &.active{
     color:white;
     background: purple;
